a18b65c56f3247a4bc5227cb38a0723917c199d1
Peteris <blankfeldspeteris@gmail.com>
2023-10-09 20:07:30 +0300

    Cv component to output CV(not styled)

---
+++ cv-application/src/components/cvRender.jsx
@@ -0,0 +1,22 @@
+
+
+function CvRender({name, lastName, email, phoneNumber, titleOfStudy, schoolName,companyName, positionTitle, responsibilities, startOfEmployment, endOfEmployment}) {
+
+    return(
+    <div>
+        <div>{name}</div>
+        <div>{lastName}</div>
+        <div>{email}</div>
+        <div>{phoneNumber}</div>
+        <div>{titleOfStudy}</div>
+        <div>{schoolName}</div>
+        <div>{companyName}</div>
+        <div>{positionTitle}</div>
+        <div>{responsibilities}</div>
+        <div>{startOfEmployment}</div>
+        <div>{endOfEmployment}</div>
+    </div>
+    )
+}
+
+export default CvRender;
