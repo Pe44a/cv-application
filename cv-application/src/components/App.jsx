bb23a5a86de65d3ef65a34bbc8c1d3cd5ecb27e2
Peteris <blankfeldspeteris@gmail.com>
2023-10-16 19:06:03 +0300

    Lightly styled cvRender

--- cv-application/src/components/App.jsx
+++ cv-application/src/components/App.jsx
@@ -18,30 +18,31 @@ function App() {
 
 
     return(<>
-        <GeneralInfo 
-         name={name}  setName={setName}
-         lastName={lastName} setLastName={setLastName}
-         email={email} setEmail={setEmail}
-         phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber}
-         schoolName={schoolName} setSchoolName={setSchoolName}/>
+        <div className="inputs">
+            <GeneralInfo 
+            name={name}  setName={setName}
+            lastName={lastName} setLastName={setLastName}
+            email={email} setEmail={setEmail}
+            phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber}
+            schoolName={schoolName} setSchoolName={setSchoolName}/>
 
-        <Education
-         titleOfStudy={titleOfStudy} setTitleOfStudy={setTitleOfStudy}
-         schoolName={schoolName} setSchoolName={setSchoolName}/>
+            <Education
+            titleOfStudy={titleOfStudy} setTitleOfStudy={setTitleOfStudy}
+            schoolName={schoolName} setSchoolName={setSchoolName}/>
 
-        <Experience
-         companyName={companyName} setCompanyName={setCompanyName}
-         positionTitle={positionTitle} setPositionTitle={setPositionTitle}
-         responsibilities={responsibilities} setResponsibilities={setResponsibilities}
-         startOfEmployment={startOfEmployment} setStartOfEmployment={setStartOfEmployment}
-         endOfEmployment={endOfEmployment} setEndOfEmployment={setEndOfEmployment}/>
-
+            <Experience
+            companyName={companyName} setCompanyName={setCompanyName}
+            positionTitle={positionTitle} setPositionTitle={setPositionTitle}
+            responsibilities={responsibilities} setResponsibilities={setResponsibilities}
+            startOfEmployment={startOfEmployment} setStartOfEmployment={setStartOfEmployment}
+            endOfEmployment={endOfEmployment} setEndOfEmployment={setEndOfEmployment}/>
+        </div>
         <CvRender
          name={name} lastName={lastName} email={email} phoneNumber={phoneNumber}
          titleOfStudy={titleOfStudy} schoolName={schoolName} companyName={companyName}
          positionTitle={positionTitle} responsibilities={responsibilities}
-         startOfEmployment={startOfEmployment} endOfEmployment={endOfEmployment}
-        />
+         startOfEmployment={startOfEmployment} endOfEmployment={endOfEmployment}/>
+
     </>)
 }
 
--- cv-application/src/components/cvRender.jsx
+++ cv-application/src/components/cvRender.jsx
@@ -3,18 +3,48 @@
 function CvRender({name, lastName, email, phoneNumber, titleOfStudy, schoolName, companyName, positionTitle, responsibilities, startOfEmployment, endOfEmployment}) {
 
     return(
-    <div>
-        <div>{name}</div>
-        <div>{lastName}</div>
-        <div>{email}</div>
-        <div>{phoneNumber}</div>
-        <div>{titleOfStudy}</div>
-        <div>{schoolName}</div>
-        <div>{companyName}</div>
-        <div>{positionTitle}</div>
-        <div>{responsibilities}</div>
-        <div>{startOfEmployment}</div>
-        <div>{endOfEmployment}</div>
+    <div className="cv">
+        <div className="header">
+            <div className="fullName"> {name } {lastName}</div>
+        </div>
+
+        <div className="navBar">
+            <div className="contacts">
+                <h1>Contacts</h1>
+
+                <ul>
+                    <li>{email}</li>
+                    <li>{phoneNumber}</li>
+                </ul>
+            </div>
+
+            <div className="education">
+                <h1>Education</h1>
+                <ul>
+                    <li>{titleOfStudy}</li>
+                    <li>{schoolName}</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="main">
+            <div className="experience">
+                <h1>Experience</h1>
+
+                <h2>Company name</h2>
+                <div className="experienceChild">{companyName}</div>
+
+                <h2>Position title</h2>
+                <div className="experienceChild">{positionTitle}</div>
+
+                <h2>Responsibilities</h2>
+                <div className="experienceChild">{responsibilities}</div>
+
+                <h2>Time of employment</h2>
+                <div className="experienceChild">Start date:{startOfEmployment}</div>
+                <div className="experienceChild">End date:{endOfEmployment}</div>
+            </div>
+        </div>
     </div>
     )
 }
